2408f5287f3291438ca82fcabf65017b23107823
Seokshin Son <snskshn@gmail.com>
2012-10-09 21:55:14 +0900

    problem 1.3.

--- ans1.py
+++ ans1.py
@@ -18,3 +18,10 @@ def print_files(path = '/'):
 			print(os.path.join(dirname, subdirname))
 		for filename in filenames:
 			print(os.path.join(dirname, filename))
+
+# 1.3
+def combination(n, r):
+	memo = [[0]*n for _ in [0]*r]
+	if r == 0 or n == r:
+		return 1
+	return combination(n - 1, r - 1) + combination(n - 1, r)
