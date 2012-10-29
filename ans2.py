5eb9c52689d8ceddf34019175b1601469170a726
Seokshin Son <snskshn@gmail.com>
2012-10-29 15:56:28 +0900

    [2.2] retain original village & added test cases.

--- ans2.py
+++ ans2.py
@@ -26,19 +26,24 @@ def findPath(L, i, j):
         return 0
     return findPath(L, i - 1, j) + findPath(L, i, j - 1)
 
-def findPathDynamic(L, i = 1, j = 1):
+def findPathDynamic(L):
+    from copy import deepcopy
+    return _findPathDynamic(deepcopy(L))
+def _findPathDynamic(L, i = 1, j = 1):
     if L[i][j] != 0:
         L[i][j] = L[i-1][j] + L[i][j-1]
     if i == j == len(L) - 1:
         return L[i][j]
     if j >= len(L) - 1:
-        return findPath_dynamic(L, i + 1, 1)
+        return _findPathDynamic(L, i + 1, 1)
     else:
-        return findPath_dynamic(L, i, j + 1)
-    
+        return _findPathDynamic(L, i, j + 1)
     
 class testFindPath(unittest.TestCase):
     def testFindPath(self):
         self.assertEqual(findPath(이상해씨마을, 4, 4), 53)
         self.assertEqual(findPath(궁금해씨마을, 4, 4), 11)
-        self.assertEqual(findPath(makeVillage(30, 30), 29, 29), 100)
+        self.assertEqual(findPathDynamic(이상해씨마을), 53)
+        self.assertEqual(findPathDynamic(궁금해씨마을), 11)
+        self.assertEqual(findPathDynamic(makeVillage(30, 30)),
+                         30067266499541040)
